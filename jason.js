6a6eceaef34989ae575bbe29979132d60d88d5c0
Robert Erady <137341984+robitiii@users.noreply.github.com>
2025-05-03 23:10:14 +0200

    Added lowerCase to alert msg string

--- jason.js
+++ jason.js
@@ -1,6 +1,6 @@
  let userInput;
  while(true){
-     userInput = prompt("Enter your task 1: ");
+     userInput = prompt( "Enter"+" Your task 1: ");
      if (userInput==null){
         alert("no tasks completed let's get to work!")// if the user presses cancel this alert message should pop out!
          continue; //back to prompt.
@@ -8,17 +8,17 @@
      
   switch (userInput){
       case "todo":
-          console.log("Title: Make correction to JSL01, status: todo");
+          console.log("'Title:' Make correction to JSL01, status: todo");
           break;
           case "doing":
-              console.log("Title: Make correction to JSL01, status: doing");
+              console.log("Title:");
               break;
               case "done":
                   console.log("Title: Make correction to JSL01, status: done");
                   break;
                   default:
-                      alert("Invalid status. Please enter 'todo', 'doing' or 'done'.");
+                 alert("INVALID STATUS. PLEASE ENTER 'TODO', 'DOING' or 'DOING'.".toLowerCase());
                       continue; //back to prompt.
   }
-  break; //only reaches here if input was vaild
+  break; //only reaches here if input was vaild.
   }
