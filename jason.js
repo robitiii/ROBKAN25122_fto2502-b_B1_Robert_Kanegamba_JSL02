d5a9dfa8e4067c731efcfdc0cb5fce04e75b9e9e
Robert Erady <137341984+robitiii@users.noreply.github.com>
2025-05-03 19:42:33 +0200

    Removed unnecessary comments

--- jason.js
+++ jason.js
@@ -16,12 +16,16 @@
   let userInput;
  while(true){
      userInput = prompt("Enter your task 1: ");
+     if (userInput==null){
+        alert("no tasks completed let's get to work!")// if the user presses cancel this alert message should pop out!
+        continue;
+     }
   switch (userInput){
       case "todo":
-          console.log("Title: Make correction to JSL01, status: done");
+          console.log("Title: Make correction to JSL01, status: todo");
           break;
           case "doing":
-              console.log("Title: Make correction to JSL01, status: done");
+              console.log("Title: Make correction to JSL01, status: doing");
               break;
               case "done":
                   console.log("Title: Make correction to JSL01, status: done");
